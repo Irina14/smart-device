601d2299a447efcb30acc348576f121dfad2efef
Irina14 <ira1786@bk.ru>
2019-10-17 22:39:36 +0600

    Добавляет форму в подвале

--- source/js/scripts/form.js
+++ source/js/scripts/form.js
@@ -1,27 +1,6 @@
 'use strict';
 
 (function () {
-  // Открытие и закрытие блоков в футоре
-  var footerBlock = document.querySelector('.footer__blocks');
-
-  if (footerBlock) {
-    var footerBlocks = footerBlock.querySelectorAll('.footer__block');
-
-    footerBlock.classList.remove('footer__blocks--nojs');
-
-    Array.prototype.slice.call(footerBlocks).forEach(function (block) {
-      var footerButton = block.querySelector('.footer__button');
-      footerButton.addEventListener('click', function () {
-        block.classList.toggle('footer__block--open');
-      });
-    });
-  }
-})();
-
-
-'use strict';
-
-(function () {
   // Валидация формы
   var form = document.querySelector('.form__question');
 
@@ -92,5 +71,3 @@
     formButton.addEventListener('click', formButtonClickHandler);
   }
 })();
-
-//# sourceMappingURL=main.js.map
